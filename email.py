d6b0ba8c49fa324d5ca21b9dd2670019a1694b5e
galla192 <galla192@msu.edu>
2016-02-06 02:11:29 -0500

    nobody reads these anyway

--- email.py
+++ email.py
@@ -1,14 +1,12 @@
-import win32com.client
+import win32com.client              #corresponing package file must be installed
 
 outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
-inbox = outlook.GetDefaultFolder(6) # "6" refers to the index of a folder - in this case,
-                                    # the inbox. You can change that number to reference
-                                    # any other folder
+inbox = outlook.GetDefaultFolder(6) #6 is the number for the inbox
+                                    #google it for more info
 messages = inbox.Items
 message = messages.GetFirst()
 body_content = message.body
-#print body_content
 print "current # of messages: "+str(len(messages))
 print "Deleting message "+str(len(messages))+"\n"+"From: "+str(message.Sender)+"\n"+"Body: "+body_content
 message.Delete()
